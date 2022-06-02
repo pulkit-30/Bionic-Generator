5d656f78922d3b87138aed640e989d0f5a8db465
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-06-02 15:24:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function convert() {
+  var str = document.getElementById('user-para').value;
+  var new_str = '';
+  str = str.split(' ');
+
+  str.forEach((word) => {
+    word = word.split('');
+    var bold_word = '';
+    var non_bold_word = '';
+    var half_len = Math.ceil(word.length / 2);
+    for (let i = 0; i < half_len; i++) {
+      bold_word += word[i];
+    }
+    for (let i = half_len; i < word.length; i++) {
+      non_bold_word += word[i];
+    }
+    new_str =
+      new_str + `<span class="bold">${bold_word}</span>` + non_bold_word + ' ';
+    console.log(new_str);
+    document.getElementById('out-para').innerHTML = new_str;
+  });
+}
+
+document.getElementById('btn').addEventListener('click', convert);
