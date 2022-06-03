0ecf9bad7fe95982795dfe26e99105eef4063b83
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-06-03 11:01:59 +0530

    fix: reduce code complexity

--- script.js
+++ script.js
@@ -1,46 +1,17 @@
 function convert() {
   var str = document.getElementById('user-para').value;
   var new_str = '';
-  str = str.split(' ');
-
-  str.forEach((word) => {
-    word = word.split('');
-    var bold_word = '';
-    var non_bold_word = '';
-    var half_len = Math.floor(word.length / 2);
-    for (let i = 0; i < half_len; i++) {
-      bold_word += word[i];
-    }
-    for (let i = half_len; i < word.length; i++) {
-      non_bold_word += word[i];
-    }
-    new_str =
-      new_str +
-      `<span class="bold" style="font-weight:bold">${bold_word}</span>` +
-      non_bold_word +
-      ' ';
-    document.getElementById('out-para').innerHTML = new_str;
+  str.replace(/\p{L}+/gu, (word) => {
+    const { length } = word;
+    let midPoint = 1;
+    if (length > 3) midPoint = Math.ceil(length / 2);
+    const firstHalf = word.slice(0, midPoint);
+    const secondHalf = word.slice(midPoint);
+    const htmlWord = `<b>${firstHalf}</b>${secondHalf}`;
+    new_str += htmlWord + ' ';
   });
+  document.getElementById('out-para').innerHTML = new_str;
+  return new_str;
 }
 
 document.addEventListener('keyup', convert);
-document.getElementById('btn').addEventListener('click', (event) => {
-  var str = document.getElementById('user-para').value;
-  var new_str = '';
-  str = str.split(' ');
-
-  str.forEach((word) => {
-    word = word.split('');
-    var bold_word = '';
-    var non_bold_word = '';
-    var half_len = Math.floor(word.length / 2);
-    for (let i = 0; i < half_len; i++) {
-      bold_word += word[i];
-    }
-    for (let i = half_len; i < word.length; i++) {
-      non_bold_word += word[i];
-    }
-    new_str = new_str + `**${bold_word}**` + non_bold_word + ' ';
-    navigator.clipboard.writeText(new_str);
-  });
-});
